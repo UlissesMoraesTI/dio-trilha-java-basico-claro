f034cfc2561973c105fdf24320a75d36772e678f
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2024-07-25 17:32:40 -0400

    feat: Desafio Banco Terminal - 3

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+
+        String nomeCliente = " ";
+        String agencia = " ";
+        int conta = 0;
+        float saldo = 0;
+
+        Scanner leitorDeEntradas = new Scanner(System.in);
+
+        System.out.println("Por favor, informe seu Nome !");
+        nomeCliente = leitorDeEntradas.next();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia = leitorDeEntradas.next();
+
+        do {
+            System.out.println("Por favor, digite o número da Conta !");
+            conta = leitorDeEntradas.nextInt();
+
+        } while (conta == 0);
+
+        System.out.println("Por favor, digite o valor do Saldo !");
+        saldo = leitorDeEntradas.nextFloat();
+
+        System.out.println(
+                "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                        + ", conta " + conta + " e seu saldo " + String.format("%.2f", saldo)
+                        + " já está disponível para saque");
+    }
+}
